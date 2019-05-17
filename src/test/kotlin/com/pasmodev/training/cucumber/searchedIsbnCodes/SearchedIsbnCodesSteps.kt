48a7da38e9c55c0f867b45c844c75f1291a2ea77
Pascual Montesinos <pascual.montesinos@liip.care>
2019-05-17 19:34:23 +0200

    Remove unused imports

--- src/test/kotlin/com/pasmodev/training/cucumber/searchedIsbnCodes/SearchedIsbnCodesSteps.kt
+++ src/test/kotlin/com/pasmodev/training/cucumber/searchedIsbnCodes/SearchedIsbnCodesSteps.kt
@@ -28,6 +28,7 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.context.ContextConfiguration
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
